2601dbfc0aa35988aead1b7f975e3b783a393d1d
RalucaZota <zotaraluca649@gmail.com>
2024-07-16 11:27:00 +0300

    changes for about section

--- src/Components/TopBar.jsx
+++ src/Components/TopBar.jsx
@@ -9,17 +9,18 @@ function TopBar() {
       <div className='topbar'>
         <div>
           <span>24/24 NON STOP</span>
-          <a href="">
-          <img src={email} alt='email' />
-          <span>
-            13carfix@gmail.com
-          </span>
+          <a href=''>
+            <img src={email} alt='email' />
+            <span>13carfix@gmail.com</span>
           </a>
         </div>
         <a target='_blank' href='https://wa.me/0720261991?'>
-          {' '}
           <img src={whatsapp} alt='whatsapp' height='35' width='35' />{' '}
+          {' '}
           <span>0720261991</span>
+        </a>
+        <a target='_blank' href='https://wa.me/0731682466?'>
+          <span>0731682466</span>
         </a>
       </div>
     </>
